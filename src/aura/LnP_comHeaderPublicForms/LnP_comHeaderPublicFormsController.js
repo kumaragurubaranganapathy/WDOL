41c2c841e3ecc2abd4fb9719148ca5c4f290099a
Shivam Sareen <shisareen@deloitte.com>
2019-05-28 17:09:59 +0530

    US -1427 & 1428

--- src/aura/LnP_comHeaderPublicForms/LnP_comHeaderPublicFormsController.js
+++ src/aura/LnP_comHeaderPublicForms/LnP_comHeaderPublicFormsController.js
@@ -1,8 +1,18 @@
 ({
 	init: function(component, event, helper) {
+	    var headerval = sessionStorage.getItem("header");
+        var conRecId= sessionStorage.getItem("contactRecId");
+        console.log('headerval'+sessionStorage.getItem("header"));
+        console.log('ConatctId'+sessionStorage.getItem("contactRecId"));
+        
+        component.set("v.headerfromsession", headerval);
+        component.set("v.conIdfromsession", conRecId);
         helper.setItemsList(component, event, helper);
-        helper.setUserInfo(component, event, helper);           
-    },
+        helper.setUserInfo(component, event, helper); 
+        
+
+     },
+  
     burgerIconOnClick: function(component, event, helper){
 		helper.burgerIconClicked(component, event);
 	},
