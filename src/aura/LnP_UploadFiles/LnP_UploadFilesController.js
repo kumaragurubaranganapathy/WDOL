23f416527650e44f84f55e47edc45f84a884db8f
SumeetKanjilal <skanjilal@deloitte.com>
2019-06-15 20:01:49 +0530

    Revert "Final Commit to CI"
    This reverts commit 6f869a2e5bb2ce1091f529d865e42d7058f013b1.

--- src/aura/LnP_UploadFiles/LnP_UploadFilesController.js
+++
@@ -1,8 +0,0 @@
-({
-	handleUploadFinished : function(component, event, helper) {
-		helper.handleUploadFinished(component, event);
-	},
-    deleteFile : function(component, event, helper) {
-        helper.deleteFile(component,event);
-    }
-})
--- src/triggers/DRE2_MUSW_License2_c.trigger
+++
@@ -1,1 +0,0 @@
-trigger DRE2_MUSW_License2_c on MUSW__License2__c (after insert, before update, before delete, after undelete) { BGCM.TriggerManager.execute('DRE2_MUSW_License2_c', new DRETriggerHandler()); }
