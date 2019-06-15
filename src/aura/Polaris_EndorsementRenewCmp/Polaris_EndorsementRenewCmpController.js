473a210c60cae50aad690e9643165a691a0588df
Sumeet Kanjilal <skanjilal@deloitte.com>
2019-06-15 14:20:35 +0000

    Deleted src

--- src/aura/Polaris_EndorsementRenewCmp/Polaris_EndorsementRenewCmpController.js
+++
@@ -1,8 +0,0 @@
-({
-	doInit : function(component, event, helper) {        
-        helper.fetchEndorsement(component, event, helper);
-    },
-    handleValueSelect : function(component, event, helper) {        
-        helper.changeStatusHelp(component, event, helper);
-    },
-})
