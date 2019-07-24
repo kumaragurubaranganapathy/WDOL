83240e82cb09c3b8bc15b56ed8aa8de039517be7
SKeskar <skeskar@deloitte.com>
2019-07-24 19:25:12 +0530

    Self Service Form Changes

--- src/aura/Self_ServiceForm/Self_ServiceFormController.js
+++ src/aura/Self_ServiceForm/Self_ServiceFormController.js
@@ -1,11 +1,18 @@
 ({
 	doInit: function(component, event, helper) { 
 		helper.doInit(component, event, helper);
+        helper.getContact(component, event, helper);
     },
     cancel: function(component, event, helper) { 
 		helper.cancel(component, event, helper);
     },
+    createcode : function(component, event, helper) { 
+		helper.createcode(component, event, helper);
+    },
     saveWebsite : function(component, event, helper) { 
 		helper.saveWebsitehelper(component, event, helper);
-    }
+    },
+    printUpdate: function(component, event, helper) { 
+		helper.printUpdatehelper(component, event, helper);
+    },
 })
