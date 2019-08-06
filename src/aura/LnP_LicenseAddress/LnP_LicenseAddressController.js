9d93767f1474b3f2a2d375aa6d104a219c3bc8bb
GunjKaur <gunjkaur@deloitte.com>
2019-08-06 14:51:06 +0530

    Making code in sync with QA
    Making code in sync with QA for LnP_LicenseAddress

--- src/aura/LnP_LicenseAddress/LnP_LicenseAddressController.js
+++ src/aura/LnP_LicenseAddress/LnP_LicenseAddressController.js
@@ -16,7 +16,12 @@
         helper.getStateList(component , helper);
         helper.getCountyList(component , helper);
         helper.getCanadianProvince(component , helper);
-        helper.getallAddress(component, event, helper);
+        if(component.get('v.changeAddress')=== true){
+            helper.getAddress(component, event, helper);  
+        } else {
+             helper.getallAddress(component, event, helper);       
+        }
+
     },
     
     addAddress: function(component, event, helper){  
@@ -178,6 +183,14 @@
         }
     },
     
+    
+    onCityChange : function(component, event, helper){
+        console.log('reaching');
+        if(component.get("v.mailingAddressparcel.MUSW__City__c") != null && component.get("v.mailingAddressparcel.MUSW__State__c") != null){
+        	//helper.countyFetchHelper(component, event, helper);
+        }
+    },
+    
     onPhysicalStateChange: function(component, event, helper){
         var selectedState = event.getSource().get("v.value");
         console.log(event.getSource().get("v.value")); 
@@ -220,7 +233,12 @@
         
     },
     onsaveAddress: function(component ,event, helper) {
-        helper.onsaveAddressHelper(component ,event, helper);
+        if(component.get("v.changeAddress")== true) {
+           helper.onsaveContactAddressHelper(component ,event, helper); 
+        } else {
+            helper.onsaveAddressHelper(component ,event, helper); 
+        }
+       
     },
     handleDelete: function(component ,event, helper) {
         helper.deleteRecordHelper(component, event, helper);
