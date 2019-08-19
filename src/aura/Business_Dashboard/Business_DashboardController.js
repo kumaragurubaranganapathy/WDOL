19fe662ce41708756b8b6e87f0ee69d67faec5fb
kojashree <kkakariya@deloitte.com>
2019-08-19 18:50:33 +0530

    Bug 5319

--- src/aura/Business_Dashboard/Business_DashboardController.js
+++ src/aura/Business_Dashboard/Business_DashboardController.js
@@ -1,12 +1,12 @@
 ({
-	doInit : function(component, event, helper) {
+    doInit : function(component, event, helper) {
         helper.setDefaultTab(component,event,helper);
-		helper.getRecordTypeId(component,event,helper);
+        helper.getRecordTypeId(component,event,helper);
         helper.fetchData(component,event, helper);
         helper.getLicenseDetails(component,event,helper);
-		var redirect = sessionStorage.getItem("fromAddbusiness");
+        var redirect = sessionStorage.getItem("fromAddbusiness");
         var businessAccountID = sessionStorage.getItem("businessAccountId");
-		if(redirect){
+        if(redirect){
             var accId = businessAccountID;
             component.set("v.selectedAccount",accId);
             helper.businessDetails(component,event,helper);
@@ -21,21 +21,21 @@
                     sessionStorage.removeItem("businessAccountId");
                 }), 1000
             );
-        }		
-	},
+        }
+    },
     doRender : function(component,event,helper){
         helper.doRender(component,event);
     },
     
     handleActive : function(component,event,helper){
-      helper.handleActive(component,event,helper);  
+        helper.handleActive(component,event,helper);  
     },
     updateBusinessInfo :function(component,event,helper){
-      helper.updateBusinessInfoHelper(component,event,helper);  
+        helper.updateBusinessInfoHelper(component,event,helper);  
     },
     
     handleLicenseLink :  function(component,event,helper){
-     /*var link = $A.get("$Label.c.Polaris_Portal_Home")+"licenseSelectionPage";
+        /*var link = $A.get("$Label.c.Polaris_Portal_Home")+"licenseSelectionPage";
         var result = component.get("v.mainAccountName");
         var evt = $A.get("e.c:Dol_FetchAccountName");
         evt.setParams({ "acccountName": result});
@@ -55,20 +55,20 @@
         var accId = event.target.id;
         component.set("v.selectedAccount",accId);
         component.set("v.accountDetail",true);
-         component.set("v.licenseDetail",false);
-         component.set("v.courseDetail",false);
+        component.set("v.licenseDetail",false);
+        component.set("v.courseDetail",false);
         component.set("v.breadcrumbLabel","courses");
-		helper.businessDetails(component,event,helper);
-		helper.setSelectedAccountData(component,event,helper,accId);
-	},
+        helper.businessDetails(component,event,helper);
+        helper.setSelectedAccountData(component,event,helper,accId);
+    },
     navigateToScreenOne : function(component, event, helper) {
-		helper.navigateToScreenOne(component,event,helper);
-	},
+        helper.navigateToScreenOne(component,event,helper);
+    },
     navigateToScreenTwo : function(component,event,helper){
         helper.navigateToScreenTwo(component,event);
     },
     licenseScreen : function(component,event,helper){
-    	helper.licenseScreen(component,event);
+        helper.licenseScreen(component,event);
     },
     setBreadCrumb : function(component,event,helper){
         var tab = event.getSource().get('v.id')
@@ -80,8 +80,8 @@
     handleComponentEvent : function(component,event,helper){
         console.log("inside handleCompnent");
         
-    	var refresh = event.getParam("refreshFlag");
-    	var isbranch = event.getParam("isbranch");
+        var refresh = event.getParam("refreshFlag");
+        var isbranch = event.getParam("isbranch");
         var licenseId = event.getParam("LicenseId");
         var primaryAccountId = event.getParam("primaryAccountId");
         console.log("licenseId::"+licenseId);
@@ -93,10 +93,10 @@
         var isdisplayLicenseDetails = event.getParam("displayLicenseDetails");
         
         
-         console.log("isDisplayCoursdetails.........."+isDisplayCoursdetails);
-         
-         console.log("isdisplayLicenseDetails.........."+isdisplayLicenseDetails);
-         
+        console.log("isDisplayCoursdetails.........."+isDisplayCoursdetails);
+        
+        console.log("isdisplayLicenseDetails.........."+isdisplayLicenseDetails);
+        
         if(isbranch){
             component.set("v.branchExam",false);
         } else{
@@ -104,8 +104,8 @@
         }
         if(refresh){
             
-        	helper.fetchData(component,event,helper);  
-        	
+            helper.fetchData(component,event,helper);  
+            
         }if(licenseId != null && licenseId !='' && isDisplayCoursdetails === true){
             
             console.log("inside coursedetails......");
@@ -118,8 +118,8 @@
             
             var tDate = new Date();
             
-        	component.set("v.todayDate",tDate);
-        	
+            component.set("v.todayDate",tDate);
+            
             helper.getCourseDetails(component,event,helper);
             
             helper.getAddressDetails(component,event,helper);
@@ -134,8 +134,8 @@
             
             var tDate = new Date();
             
-        	component.set("v.todayDate",tDate);
-        	
+            component.set("v.todayDate",tDate);
+            
             helper.getLicenseDetails(component,event,helper);
             
             helper.getEndorsementDetails(component,event,helper);
@@ -143,29 +143,29 @@
             helper.getAddressDetails(component,event,helper);
         }
         console.log('inside refresh::');
-	},
+    },
     updateContactInfo: function(component, event, helper) {
-		helper.updateContactInfo(component, event, helper);
-	},
+        helper.updateContactInfo(component, event, helper);
+    },
     getAccountName : function (component,event,helper) {
-      helper.getAccountName(component, event, helper);  
+        helper.getAccountName(component, event, helper);  
     },
     BillingCode : function (component,event,helper) {
-      helper.BillingCode(component, event, helper);  
+        helper.BillingCode(component, event, helper);  
     },
     showMoreActions : function(component,event,helper){
         helper.showMoreActions(component,event);
     },
     showMoreDashboard : function(component,event,helper){
-    	helper.showMoreDashboard(component,event);
+        helper.showMoreDashboard(component,event);
     },
     showMoreAMR : function(component,event,helper){
-    	helper.showMoreAMR(component,event);
+        helper.showMoreAMR(component,event);
     },
     addBusiness : function(component,event,helper){
         helper.addBusiness(component,event);
     },
     goToCart : function(component,event,helper){
         helper.goToCart(component,event);
-    }   
+    }
 })
