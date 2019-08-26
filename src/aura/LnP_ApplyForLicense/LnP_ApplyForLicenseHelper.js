9a3c482afdd3bcb2c65feb299d9a00bb029138e8
snagarkar <snagarkar@deloitte.com>
2019-08-26 18:22:52 +0530

    Bug 6467

--- src/aura/LnP_ApplyForLicense/LnP_ApplyForLicenseHelper.js
+++ src/aura/LnP_ApplyForLicense/LnP_ApplyForLicenseHelper.js
@@ -350,10 +350,10 @@
         if(component.get("v.licenseType")=='Notary Public' && tabsList[currentTab-1].labelFieldsMap[questionNumber].messageTriggerResponse == response)
         {
             component.set("v.showNotaryEndo",true);
-            component.set("v.showEndoMessage",true);
-            component.set("v.endoMessage",tabsList[currentTab-1].labelFieldsMap[questionNumber].message);  
+            //component.set("v.showEndoMessage",true);
+            //component.set("v.endoMessage",tabsList[currentTab-1].labelFieldsMap[questionNumber].message);  
         }
-        else if(tabsList[currentTab-1].labelFieldsMap[questionNumber].messageTriggerResponse == response)
+        else if(tabsList[currentTab-1].labelFieldsMap[questionNumber].messageTriggerResponse == response && component.get("v.licenseType")!='Notary Public')
         {            
             component.set("v.showEndoMessage",true);
             component.set("v.endoMessage",tabsList[currentTab-1].labelFieldsMap[questionNumber].message);            
