147504f0bc6616a0e7d255755e0dc6574abbd3dc
bchaitanyasubbara <bchaitanyasubbara@deloitte.com>
2019-08-30 16:27:54 +0530

    BUG-5108

--- src/aura/Polaris_RenewLicenseController/Polaris_RenewLicenseControllerController.js
+++ src/aura/Polaris_RenewLicenseController/Polaris_RenewLicenseControllerController.js
@@ -1,5 +1,19 @@
 ({
 	doInit : function(component, event, helper) {
+		window.onbeforeunload = function(e) {
+          e.preventDefault();
+          e.returnValue = '';
+        };
+        if(window.performance.navigation.type == 1) {
+            //event.preventDefault();
+            var url=$A.get("$Label.c.Polaris_Portal_Home")+'explorer-error-page';
+            var urlEvent = $A.get("e.force:navigateToURL");
+            urlEvent.setParams({
+                "url": url
+            });
+            urlEvent.fire();
+            return false;
+        }
         console.log('inside init');
         helper.setApplicationType(component, event, helper);
         helper.fetchDataFromServer(component, event, helper); 
@@ -41,7 +55,8 @@
      attestValue: function(component, event, helper) {        
         alert(event.getSource().get("v.value"));
     },
-    submit : function(component, event, helper) {        
+    submit : function(component, event, helper) { 
+		window.onbeforeunload = null;      
         helper.submit(component, event, helper);
     },
      handlePassValuesEvent :  function(component, event, helper){
