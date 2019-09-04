87719355e254d02e8d31a40e0082f549191a41c4
SKeskar <skeskar@deloitte.com>
2019-09-04 14:59:16 +0530

    IE page fix

--- src/aura/LnP_ThemeLayoutMain/LnP_ThemeLayoutMainHelper.js
+++ src/aura/LnP_ThemeLayoutMain/LnP_ThemeLayoutMainHelper.js
@@ -5,7 +5,7 @@
         var isIE = browserUsed.indexOf("MSIE ") > -1 || browserUsed.indexOf("Trident/") > -1;
         console.log(isIE + " = IE Browser");
         if(isIE && !url.includes('explorer-error-page')){
-            //window.location.href("https://dev-polaris.cs32.force.com/lightningwashington/s/explorer-error-page?IE-error");
+          	//window.location.href("https://dev-polaris.cs32.force.com/lightningwashington/s/explorer-error-page?IE-error");
            
             window.location.href($A.get("$Label.c.Polaris_Portal_Home") + 'explorer-error-page?IE-error');/*var str = '/explorer-error-page?IE-error';
             var urlEvent = $A.get("e.force:navigateToURL");
@@ -15,11 +15,12 @@
             urlEvent.fire();*/
         }
     },
-	 closeServiceMenu : function(component,event){
+    
+    closeServiceMenu : function(component,event){
         try{
             var header=component.find("header");            
-         	var target=event.target;
-          
+            var target=event.target;
+            
             if(target){
                 
                 if(target.className!=="service-wrapper"&&target.className!=='service'){                                      
@@ -29,7 +30,7 @@
             }
             
             
-      
+            
             
         }catch(e){
             console.log(e.stack,true);
--- src/aura/LnP_userThemePublicFormsLayout/LnP_userThemePublicFormsLayoutHelper.js
+++ src/aura/LnP_userThemePublicFormsLayout/LnP_userThemePublicFormsLayoutHelper.js
@@ -1,16 +1,30 @@
 ({
+    checkForBrowser : function(component,event){
+        var browserUsed = window.navigator.userAgent;
+        var isIE = browserUsed.indexOf("MSIE ") > -1 || browserUsed.indexOf("Trident/") > -1;
+        if(isIE && !url.includes('explorer-error-page')){
+            //window.location.href("https://dev-polaris.cs32.force.com/lightningwashington/s/explorer-error-page?IE-error");
+         
+          window.location.href($A.get("$Label.c.Polaris_Portal_Home") + 'explorer-error-page?IE-error');/*var str = '/explorer-error-page?IE-error';
+          var urlEvent = $A.get("e.force:navigateToURL");
+          urlEvent.setParams({
+              "url": str
+          });
+          urlEvent.fire();*/
+      }
         
-	/*showNavigation : function(component, event, helper) {
-		var pageTitle = window.location.href;
+    },
+    /*showNavigation : function(component, event, helper) {
+        var pageTitle = window.location.href;
         if(pageTitle.includes('dashboard')){
-           component.set("v.showSideNav", true);
-       }	
+            component.set("v.showSideNav", true);
+        }	
     }, */
     setBackground : function(component, event, helper){
         var pageTitle=window.location.href;
         if(pageTitle.includes('wizard')){
             component.set("v.pageTitle","wizard");
-         
+            
         }
     }
 })
