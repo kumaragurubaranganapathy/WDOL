b20037fb2f07b1f612af64e10e1730f95b7e79ba
Ananth <apillutla@deloitte.com>
2019-09-10 09:11:24 -0700

    Update

--- src/aura/LnP_userThemePublicFormsLayout/LnP_userThemePublicFormsLayoutController.js
+++
@@ -1,8 +0,0 @@
-({
-	init : function(component, event, helper) {
-        helper.checkForBrowser(component,event);
-		//helper.showNavigation(component, event, helper);
-        helper.setBackground(component, event, helper);
-        
-	}
-})
