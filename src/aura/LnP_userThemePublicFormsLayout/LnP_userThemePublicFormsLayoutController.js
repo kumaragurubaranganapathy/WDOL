07f8b78db7de27d2936a1ff2b8694e0c2418604e
vagoel <vagoel@deloitte.com>
2019-09-12 17:16:21 +0530

    repo sync
    repo sync

---
+++ src/aura/LnP_userThemePublicFormsLayout/LnP_userThemePublicFormsLayoutController.js
@@ -0,0 +1,8 @@
+({
+	init : function(component, event, helper) {
+        helper.checkForBrowser(component,event);
+		//helper.showNavigation(component, event, helper);
+        helper.setBackground(component, event, helper);
+        
+	}
+})
