2f25c2fc01c84ab382163d7aaf86a1281a3c11ef
Swain, Nibedita (US - Denver) <nswain@deloitte.com>
2019-09-18 18:07:21 -0700

    Kofax Upload-4th transaction

--- src/aura/Dol_UploadBackofficeDocumnetOnSubmission/Dol_UploadBackofficeDocumnetOnSubmissionController.js
+++ src/aura/Dol_UploadBackofficeDocumnetOnSubmission/Dol_UploadBackofficeDocumnetOnSubmissionController.js
@@ -2,13 +2,25 @@
 	doInit : function(component, event, helper) {
 		helper.loadUser(component, event, helper);
         helper.checkProfessionCode(component, event, helper);
+        helper.checkImageUrlExists(component, event, helper);
+        helper.getimagetypeOptions(component, event, helper);
 	},
     
     uplaod: function(component, event, helper){
         var recordId = component.get("v.recordId");
         var title = component.get("v.title");
         var docName = component.get("v.docName");
-        var userName = component.get("v.userName");
+        var userName = component.get("v.userEmail");
+        var selectedimage = component.find("imageType");
+        var imagetype = selectedimage.get("v.value");
+        
+        console.log('title'+title);
+        console.log('docName'+docName);
+        console.log('userName'+userName);
+        console.log('imagetype'+imagetype);
+        var archDate = component.get("v.archieveDate");
+        console.log('archDate'+archDate);
+           
         var valid = true;
         
         if(title == null || title ==""){
@@ -17,22 +29,37 @@
         if(docName == null || docName ==""){
             valid = false;
         }
+        if(imagetype == null || imagetype ==""){
+            valid = false;
+        }
         if(!valid){
              helper.showToast(component, event, "Error!", "error", "Please fill in all required fields.");
         }
         else{
+            var imageUrl = component.get("v.ImageUrlValue");
+         	console.log('imageUrl=='+imageUrl);
+
+            if(imageUrl === 'SANImageURLExists'){
+                helper.showToast(component, event, "Error!", "error", "Please delete the existing SAN Image URL before uploading a new one");
+                helper.setDefaultFields(component);
+            }
             var profCode = component.get("v.profCodeValue");
-            
             if(profCode === 'NoProfessionCode'){
                 helper.showToast(component, event, "Error!", "error", "Professional code associated to the Submission License is NULL");
                 helper.setDefaultFields(component);
             }
-            else if(profCode === 'ProfessionCodeExists'){
+            if(imagetype === 'Compliance' && (archDate === undefined || archDate === null)){
+                helper.showToast(component, event, "Error!", "error", "Archieve Date can not be empty for Compliance");
+                helper.setDefaultFields(component);
+            }
+            else if(profCode === 'ProfessionCodeExists' && imageUrl != 'SANImageURLExists'){
             var action = component.get("c.backofficeUplaod");
        		 action.setParams({
-            "recordId" : 'recordId',
-            "docName": 'docName',
-            "user": 'userName',
+            "recordId" : recordId,
+            "docName": docName,
+            "user": userName,
+            "imagetype": imagetype,
+            "archDate" : archDate,   
             }) ; 
             component.set("v.Spinner", true);
         	action.setCallback(this, function(response){
@@ -53,7 +80,8 @@
                        	helper.refreshRecords();
                     	helper.closeQuickAction(component, event, helper);
                     }
-                } if(status === "ERROR"){
+                }
+                if(status === "ERROR"){
                 var error = response.getError();
                 console.log('some problem'+error[0].message);
                 helper.showToast(component, event, "Error!", "error", "There is some error.");
@@ -62,10 +90,23 @@
                 helper.closeQuickAction(component, event, helper);
                 }
         	});
-        }
-        $A.enqueueAction(action);  
+                $A.enqueueAction(action);
+        	}  
         }       
     },
+    onImageTypeChange : function(component, event, helper){
+        component.set("v.displayArchieveDate" , false);
+        //Get Value of Deposit Picklist
+        var selectCmp = component.find("imageType");
+        var value = selectCmp.get("v.value");
+        console.log("onImageTypeChange=="+value);
+        if(value == 'Compliance'){
+            component.set("v.displayArchieveDate" , true);  
+            var displayArchieveDate = true;
+            console.log("displayArchieveDate=="+displayArchieveDate);
+        }
+        component.set("v.sanImageType" , value);   
+    }, 
     handleCancel : function(component, event, helper) {
         $A.get("e.force:closeQuickAction").fire();
     },
--- src/aura/Dol_UploadBackofficeDocumnetOnSubmission/Dol_UploadBackofficeDocumnetOnSubmissionHelper.js
+++ src/aura/Dol_UploadBackofficeDocumnetOnSubmission/Dol_UploadBackofficeDocumnetOnSubmissionHelper.js
@@ -6,7 +6,13 @@
                 var result = response.getReturnValue();
                 if(result!= null && result.Name!= null){
                     component.set("v.userName" ,result.Name ); 
-                    
+                    var email = result.Email;
+                    if(email.includes('@')){
+                        var emaiaddress = email.split('@');
+        				var emailWithoutDomain = emaiaddress[0];
+                        console.log('emailWithoutDomain'+emailWithoutDomain);
+                        component.set("v.userEmail" ,emailWithoutDomain ); 
+                    }
                 }            
         	}           
       });
@@ -29,7 +35,29 @@
                 if(result!= null && result === 'ProfessionCodeExists'){
                     component.set("v.profCodeValue", 'ProfessionCodeExists');
                 } 
-            } if(response.getState() === "ERROR"){
+            } 
+            if(response.getState() === "ERROR"){
+                var error = response.getError();
+                console.log('some problem'+error[0].message);}
+    
+      });
+    $A.enqueueAction(action);
+	},
+    checkImageUrlExists : function(component, event, helper) {	
+        var recordId = component.get("v.recordId");
+		var action = component.get("c.getImageUrl");
+        action.setParams({
+            "recordId" : recordId
+            }) ; 
+       	action.setCallback(this, function(response){
+        	if(response.getState() === "SUCCESS") {
+                var result = response.getReturnValue();
+                console.log('checkImageUrlExists result=='+result);
+                if(result!= null && result === 'SANImageURLExists'){
+                    component.set("v.ImageUrlValue", 'SANImageURLExists');
+                }
+            } 
+            if(response.getState() === "ERROR"){
                 var error = response.getError();
                 console.log('some problem'+error[0].message);}
     
@@ -50,6 +78,8 @@
     setDefaultFields: function(component) {
         component.set("v.title", "");
         component.set("v.docName", "");
+        component.set("v.submission.SAN_Image_Type__c", ""); 
+        
     },
     refreshRecords : function(component, event, helper) {
         $A.get('e.force:refreshView').fire();
@@ -57,5 +87,36 @@
     closeQuickAction : function(component, event, helper) {
         var dismissActionPanel = $A.get("e.force:closeQuickAction"); 
 		dismissActionPanel.fire(); 
+    },
+     getimagetypeOptions: function(component, helper){
+        var action = component.get("c.getSelectOptionValues");
+        var submission  = component.get("v.submission");
+        action.setParams({
+            'objObject': submission,
+            'fld': 'SAN_Image_Type__c'
+        });
+        var optsimageType = [];
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {              
+                var imageTypeValue = response.getReturnValue();
+                if (imageTypeValue != undefined && imageTypeValue.length > 0) {
+                    optsimageType.push({
+                        class: "optionClass",
+                        label: "--- None ---",
+                        value: ""
+                    });
+                }
+                for (var i = 0; i < imageTypeValue.length; i++) {
+                    optsimageType.push({
+                        class: "optionClass",
+                        label: imageTypeValue[i],
+                        value: imageTypeValue[i]
+                    });
+                }
+                component.find("imageType").set("v.options", optsimageType);  
+            } 
+        });
+        $A.enqueueAction(action); 
     },
 })
