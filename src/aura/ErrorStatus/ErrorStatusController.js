c021219ef887bc8b4b31a17a37b84f4b00aff194
kumaragurubaranganapathy <kganapathy@deloitte.com>
2019-10-08 23:29:24 -0600

    UAT Build error fix

---
+++ src/aura/ErrorStatus/ErrorStatusController.js
@@ -0,0 +1,11 @@
+({
+	init : function(component, event, helper) {
+        helper.init(component, event);
+	},
+    close: function(component, event, helper) {
+        helper.close(component, event);
+	},
+    handleAlertClose : function(component,event,helper){
+        helper.handleAlertClose(component,event);
+    }
+})
